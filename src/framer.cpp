684a78182e52883ebbbe506bf219944daa0f55fc
Đorđe Jocić <office@djordjejocic.com>
2018-11-19 05:52:10 +0100

    Added license to the project.

--- src/framer.cpp
+++ src/framer.cpp
@@ -1,9 +1,10 @@
 /***********************************************************\
-|* Author: Djordje Jocic <office@djordjejocic.com>         *|
-|* Year: 2018                                              *|
-|* ------------------------------------------------------- *|
-|* Copyright (C) 2018                                      *|
-|* ------------------------------------------------------- *|
+|* Project Author: Djordje Jocic                           *|
+|* Project Year: 2018                                      *|
+|* Project License: GNU Affero General Public License v3   *|
+|* ======================================================= *|
+|* Personal Website: http://www.djordjejocic.com/          *|
+|* ======================================================= *|
 |* This program is free software: you can redistribute     *|
 |* it and/or modify it under the terms of the GNU Affero   *|
 |* General Public License as published by the Free         *|
@@ -13,10 +14,11 @@
 |* This program is distributed in the hope that it will    *|
 |* be useful, but WITHOUT ANY WARRANTY; without even the   *|
 |* implied warranty of MERCHANTABILITY or FITNESS FOR A    *|
-|* PARTICULAR PURPOSE.  See the GNU Affero General Public  *|
-|* License for more details. You should have received a    *|
-|* copy of the GNU Affero General Public License along     *|
-|* with this program.                                      *|
+|* PARTICULAR PURPOSE.                                     *|
+|* ------------------------------------------------------- *|
+|* See the GNU Affero General Public License for more      *|
+|* details. You should have received a copy of the GNU     *|
+|* Affero General Public License along with this program.  *|
 |* ------------------------------------------------------- *|
 |* If not, see <http://www.gnu.org/licenses/>.             *|
 |* ------------------------------------------------------- *|
